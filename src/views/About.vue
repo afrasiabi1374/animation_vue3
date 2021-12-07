7dd8ac7a077a44b60cbd119158894eb9732aac68
afrasiabi1374 <mohammad.aligiga@gmail.com>
2021-12-07 12:14:24 +0330

    anime

---
+++ src/views/About.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="about">
+    <transition appear name="fade"
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+    <h1>About</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup() {
+    const showTitle = ref(true)
+    const beforeEnter = (el) => {
+      console.log('beforeEnter',el)
+    }
+    const enter = (el) => {
+      console.log('enter',el)
+    }
+    const afterEnter = (el) => {
+      el.style.color = 'green'
+      console.log('afterEnter',el)
+      setTimeout(()=>showTitle.value = false , 2000)
+    }
+    const beforeLeave = (el) =>{
+      el.style.color = 'pink'
+      console.log('beforeLeave',el)
+    }
+    const leave = (el) =>{
+      console.log('leave',el)
+    }
+    const afterLeave = (el) =>{
+      console.log('afterLeave',el)
+    }
+    return {
+      leave,
+      afterLeave,
+      beforeLeave,
+      beforeEnter,
+      enter,
+      afterEnter,
+      showTitle
+    }
+  }
+}
+</script>
+
+<style>
+  .about {
+    max-width: 600px;
+    margin: 20px auto;
+  }
+  .fade-enter-from{
+    opacity: 0;
+  }
+  .fade-enter-active{
+    transition: opacity 3s ease;
+  }
+  .fade-leave-to{
+    opacity: 0;
+  }
+  .fade-leave-active{
+    transition: opacity 3s ease;
+  }
+</style>
---
+++ src/views/training.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+      <transition>
+      <h1 v-if="myVar">we have this products</h1>
+      <h1 v-else>we dont have this products</h1>
+      </transition>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
